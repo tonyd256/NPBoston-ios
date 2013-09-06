edd0304327a0057bcd65f01bdaacd992bc96e367
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2013-09-06 13:51:59 -0400

    Remove error handling from NPUtils class

--- NovProject/helpers/NPUtils.h
+++ NovProject/helpers/NPUtils.h
@@ -6,12 +6,7 @@
 //  Copyright (c) 2013 Tony DiPasquale. All rights reserved.
 //
 
-@class AFJSONRequestOperation;
-
 @interface NPUtils : NSObject
-
-//+ (NSString *)reportError:(NSError *)error WithMessage:(NSString *)message FromOperation:(AFJSONRequestOperation *)operation;
-//+ (NSString *)reportError:(NSError *)error WithMessage:(NSString *)message;
 
 + (NSString *)timeToString:(NSNumber *)timeObj;
 + (NSNumber *)stringToTime:(NSString *)timeStr;
